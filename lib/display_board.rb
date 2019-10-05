938a8aeb56c7a736a0c1feeab5f71af108b956ff
Damjan Stefanovic <damjanstefanovic@gmail.com>
2019-10-05 20:22:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   | #{board[4]}  |   "
+  puts "   |#{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
